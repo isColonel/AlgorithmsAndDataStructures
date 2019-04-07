0df793f6b0b45c499c5632e8fdcb5b16478f956e
lianyubo <lianyubo>
2019-04-07 19:11:17 +0800

    kgd

--- src/algorithms/tenbase/kgd/HeapSort.java
+++ src/algorithms/tenbase/kgd/HeapSort.java
@@ -1,4 +1,55 @@
 package algorithms.tenbase.kgd;
 
+import java.util.Arrays;
+
 public class HeapSort {
+    public static void  sort(int []arr){
+        //1.构建大顶堆
+        for(int i = arr.length/2-1; i >= 0; i--){
+            //从第一个非叶子结点从下往上，从右到左调整结构
+            adjustHeap(arr,i,arr.length);
+
+        }
+        //2.调整堆结构+交换堆顶元素与末尾元素
+        for(int j=arr.length-1; j>0; j--){
+            swap(arr,0,j);
+            adjustHeap(arr,0,j);
+        }
+    }
+
+    //调整大顶堆（仅是调整过程，建立在大顶堆已构建的基础上）
+    public static void adjustHeap(int []arr, int i, int length){
+        //先取出当前元素i
+        int temp = arr[i];
+        //从i结点的左子结点开始，也就是2i+1处开始
+        for(int k=i*2+1; k<length; k=k*2+1){
+            //如果左子结点小于右子结点，k指向右子结点
+            if(k+1<length && arr[k]<arr[k+1]){
+                k++;
+            }
+            if(arr[k] > temp){
+                arr[i] = arr[k];
+                i = k;
+            }else {
+                break;
+            }
+        }
+        arr[i] = temp;
+    }
+
+    //交换元素
+    public static void  swap(int[] arr, int a, int b){
+        int temp = arr[a];
+        arr[a] = arr[b];
+        arr[b] = temp;
+    }
+
+
+    public static void main(String []args){
+        int []arr = {9,8,7,6,5,4,3,2,1};
+        sort(arr);
+        //不能直接输出数组，打印出来的是数组的地址
+        System.out.println(Arrays.toString(arr));
+    }
+
 }
--- src/algorithms/tenbase/kgd/MergeSort.java
+++ src/algorithms/tenbase/kgd/MergeSort.java
@@ -1,4 +1,38 @@
 package algorithms.tenbase.kgd;
 
 public class MergeSort {
+    //非递归算法为
+    public static int[] sort(int[] a,int[] b){
+        int[] c = new int[a.length+b.length];
+        int aNum = 0,bNum = 0, cNum =0;
+        while (aNum<a.length && bNum <b.length){
+            //比较数组a和数组b的元素，谁更小将谁赋值到c数组
+            if (a[aNum] >= b[bNum]){
+                c[cNum++] = b[bNum++];
+            }else {
+                c[cNum++] = a[aNum++];
+            }
+        }
+        //如果数组a全部赋值到数组c了，但是b数组还有元素，则将b数组剩余的元素按照顺序全部赋值到c数组
+        while(aNum == a.length && bNum < b.length){
+            c[cNum++] = b[bNum++];
+        }
+        //如果数组b全部赋值到数组c了，但数组a还有元素，则将a数组剩余的元素按照顺序全部赋值到c数组
+        while (bNum == b.length && aNum < a.length){
+            c[cNum++] = a[aNum++];
+        }
+        return c;
+    }
+
+    //测试
+    public static void main(String[] args) {
+        int[] arrayC ;
+        int[] arrayA = {2,4,6,8};
+        int[] arrayB = {1,3,5,7,9};
+        arrayC = sort(arrayA,arrayB);
+        for (int i : arrayC) {
+            System.out.print(i + " ");
+        }
+    }
+
 }
